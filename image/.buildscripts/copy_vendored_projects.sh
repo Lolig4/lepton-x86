2f89a0683f5a0aa5540ff8107640ea3dfd7faf70
Franz-Josef Haider <frajo@frajo.fi>
2026-09-15 19:46:16 +0300

    Change submodules to public repos.

--- image/.buildscripts/copy_vendored_projects.sh
+++ image/.buildscripts/copy_vendored_projects.sh
@@ -7,6 +7,6 @@ ANDROID_ROOT="${ROOT_DIR}/output"
 
 # Force-copy over the contents of our vendored projects, so that local changes
 # are reflected without needing to push changes up to the actual repositories.
-rsync -Pa --delete "${ROOT_DIR}/android_device_valve_waydroid/" "${ANDROID_ROOT}/device/waydroid/waydroid"
+rsync -Pa --delete "${ROOT_DIR}/android_device_waydroid_waydroid/" "${ANDROID_ROOT}/device/waydroid/waydroid"
 rsync -Pa --delete "${ROOT_DIR}/android_hardware_waydroid/" "${ANDROID_ROOT}/hardware/waydroid"
 rsync -Pa "${ROOT_DIR}/android_vendor_valve/" "${ANDROID_ROOT}/vendor/extra"
